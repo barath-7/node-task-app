a3457e6fcf517bd6cde722f6bead7a54205746a2
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-05-12 23:38:42 +0530

    Added code to populate user data

--- src/models/task.js
+++ src/models/task.js
@@ -9,6 +9,11 @@ const Tasks = mongoose.model('Tasks',{
         type:Boolean,
         required:false,
         default:false
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
     }
 })
 
