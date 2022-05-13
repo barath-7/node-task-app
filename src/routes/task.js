b087b8856db9049cd9a29f25e892f51f0520ed2c
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-05-13 20:57:52 +0530

    Added authentication to task routes

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,24 +1,34 @@
 const express = require("express");
+const authMiddleware = require("../middleware/authMiddleware");
 const Tasks = require('../models/task')
 const router = express.Router();
 
-router.post("/tasks", async (req, res) => {
-  let task = new Tasks(req.body);
+router.post("/tasks",authMiddleware, async (req, res) => {
+  // let task = new Tasks(req.body);
+  let task = new Tasks({
+    ...req.body,
+    owner:req.user._id
+  });
 
   try {
     await task.save();
-    res.send(`Task saved succesfully${task}`);
+    res.send(task);
   } catch (error) {
-    res.status(400).send(`Error - ${error}`);
+    res.status(500).send(error.message);
   }
 });
 
 //get all tasks
+//changed it to get the tasks created by ech individual user
 
-router.get("/all/tasks", async (req, res) => {
+router.get("/all/tasks", authMiddleware,async (req, res) => {
   try {
-    let tasks = await Tasks.find({});
+    // let tasks = await Tasks.find({});
+
+    let tasks = await Tasks.find({owner:req.user._id});
     res.send(tasks);
+    // await req.user.populate('tasks')
+    // res.send(req.user.tasks)
   } catch (error) {
     res.status(500).send(`Internal server error ${error}`);
   }
@@ -26,12 +36,16 @@ router.get("/all/tasks", async (req, res) => {
 
 //get task by id
 
-router.get("/task/:id", async (req, res) => {
+router.get("/task/:id", authMiddleware,async (req, res) => {
   // console.log(req.params.id)
   let id = req.params.id;
 
   try {
-    let task = await Tasks.findById(id).exec();
+    // let task = await Tasks.findById(id).exec();
+    let task = await Tasks.findOne({_id:id,owner:req.user._id})
+    if(!task){
+      return res.status(400).send('No Task found')
+    }
     res.send(task);
     //    console.log(data)
   } catch (error) {}
@@ -39,7 +53,7 @@ router.get("/task/:id", async (req, res) => {
 
 //update task by id
 
-router.patch("/update/task/:id", async (req, res) => {
+router.patch("/update/task/:id",authMiddleware, async (req, res) => {
   let updates = Object.keys(req.body);
   let allowedUpdates = ["description", "completed"];
   let isValidUpdate = updates.every((update) =>
@@ -55,12 +69,15 @@ router.patch("/update/task/:id", async (req, res) => {
     //   runValidators: true,
     //   new: true,
     // });
-    let task = await Tasks.findById(id)
+    // let task = await Tasks.findById(id)
+    let task = await Tasks.findOne({_id:id,owner:req.user._id})
+    if (!task) {
+      return res.status(404).send("Task not found");
+    }
+    // console.log(task)
     updates.forEach(update=>task[update]=req.body[update])
     await task.save()
-    if (!task) {
-      res.status(404).send("Task not found");
-    }
+    
     res.send(task);
   } catch (error) {
     res.status(500).send(error);
@@ -69,11 +86,12 @@ router.patch("/update/task/:id", async (req, res) => {
 
 //delete task by id
 
-router.delete("/delete/task/:id", async (req, res) => {
+router.delete("/delete/task/:id", authMiddleware,async (req, res) => {
   let id = req.params.id;
 
   try {
-    let deletedTask = await Tasks.findByIdAndDelete(id);
+    // let deletedTask = await Tasks.findByIdAndDelete(id);
+    let deletedTask = await Tasks.findOneAndDelete({_id:id,owner:req.user._id});
     if (!deletedTask) {
       return res.status(404).send("Task not found");
     }
